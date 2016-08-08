9b7f13f73b7f9027638b12d44c6a78a7befa8c8e
diegofrasal <diegofrasal@gmail.com>
2016-08-08 11:02:55 -0400

    Commit message for adding a new line.

--- Test.py
+++ Test.py
@@ -1,1 +1,9 @@
+# Databricks notebook source exported at Mon, 8 Aug 2016 15:02:34 UTC
 print "Hello World"
+
+# COMMAND ----------
+
+print "Let's add a line to the python file"
+
+# COMMAND ----------
+
